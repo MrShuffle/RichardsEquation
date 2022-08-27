c8e5fc4f505b7786c933325f433a60f9af3d38ca
MrShuffle <71754696+MrShuffle@users.noreply.github.com>
2022-08-27 21:32:11 +0200

    modifed local evaluation
    should be changed at a later time such that L scheme does not need the derivative

--- RichardsEqFEM/source/localevaluation/local_evaluation.py
+++ RichardsEqFEM/source/localevaluation/local_evaluation.py
@@ -48,26 +48,44 @@ class localelement_function_evaluation():
         # self.theta_prime = sp.diff(self.theta_func(x),x)
         # # Derivative of K wrt theta
         # self.K_prime = sp.diff(self.K_func(x),x)
-       
-        
-        
+        quadrature = gauss_quadrature_points(P_El.degree +1)
+        quadrature_points = quadrature[:,0:2]
+        Phi = P_El.phi_eval(quadrature_points)
+        dPhi = P_El.grad_phi_eval(quadrature_points)
+        self.val_Q = np.zeros((len(Phi),1))# np.dot(self.val.T.reshape(len(self.val)),Phi.T)
+        self.valgrad_Q = np.zeros((dPhi.shape[1],dPhi.shape[2]))
+        # sum over quadrature points q
+        for q in range(len(Phi)):
+            rt = 0
+            gradrt =0
+            # sum over local index
+            for k in range(len(self.val)):
+                rt += self.val[k]*Phi[q][k].T
+                gradrt += self.val[k]*dPhi[q][k]
+                #print(gradrt)
+            self.val_Q[q] = rt
+            self.valgrad_Q[q] = gradrt
+            #print(self.valgrad_Q[q],self.valgrad_Q)
+        #print(self.val_Q)
         self.theta = np.zeros((len(u),1))
         self.K = np.zeros((len(u),1))
         self.theta_d_psi = np.zeros((len(u),1))
         self.K_d_theta = np.zeros((len(u),1))
         self.K_d_psi = np.zeros((len(u),1))
         for k in range(len(u)):
-            self.theta[k] = self.theta_func(self.val[k])
-            self.K[k]     = self.K_func(self.theta[k])
-    
-            z = self.theta_prime_func.subs(x,self.val[k].item())
-            r = self.K_prime_func.subs(x,self.theta[k].item())
+            self.theta[k] = self.theta_func(self.val_Q[k].item())
+            #self.K[k]     = self.K_func(self.theta[k].item())
+            #print(self.val[k])
+            self.K[k]     = self.K_func(self.val_Q[k].item())
+            r = self.K_prime_func.subs(x,self.val_Q[k].item())
+            z = self.theta_prime_func.subs(x,self.val_Q[k].item())
+            #r = self.K_prime_func.subs(x,self.theta[k].item())
 
             self.theta_d_psi[k] = z#self.theta_prime_func.evalf(subs={x: np.asscalar(self.val[k])})
           
             self.K_d_theta[k] = r #self.K_prime_func(self.theta[k])
             # derivative of K wrt psi
-            self.K_d_psi[k] = self.K_d_theta[k]*self.theta_d_psi[k]
+            self.K_d_psi[k] = self.K_d_theta[k]#*self.theta_d_psi[k]
             
         self.interpolate_to_quadpts(P_El) # interpolates K and theta on a local element to quadtarure points
     
@@ -75,10 +93,10 @@ class localelement_function_evaluation():
         quadrature = gauss_quadrature_points(P_El.degree +1)
         quadrature_points = quadrature[:,0:2]
         Phi = P_El.phi_eval(quadrature_points)
-        self.K_in_Q = np.dot(self.K.T.reshape(len(self.K)),Phi.T)
-        self.theta_in_Q = np.dot(self.theta.T.reshape(len(self.theta)),Phi.T)
-        self.K_prime_Q = np.dot(self.K_d_psi.T.reshape(len(self.K)),Phi.T)
-        self.theta_prime_Q = np.dot(self.theta_d_psi.T.reshape(len(self.theta)),Phi.T)
+        self.K_in_Q = self.K#np.dot(self.K.T.reshape(len(self.K)),Phi.T)
+        self.theta_in_Q = self.theta#np.dot(self.theta.T.reshape(len(self.theta)),Phi.T)
+        self.K_prime_Q = self.K_d_psi#np.dot(self.K_d_psi.T.reshape(len(self.K)),Phi)
+        self.theta_prime_Q = self.theta_d_psi#np.dot(self.theta_d_psi.T.reshape(len(self.theta)),Phi.T)
         
         
         
