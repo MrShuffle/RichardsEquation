ea576ea014fbe46912f6a9efc93e6c0625c1920d
jsstokke <71754696+MrShuffle@users.noreply.github.com>
2022-12-13 12:02:33 +0100

    Add files via upload

--- RichardsEqFEM/source/localevaluation/local_evaluation.py
+++ RichardsEqFEM/source/localevaluation/local_evaluation.py
@@ -12,7 +12,7 @@ from sympy.tensor.array import derive_by_array
 from sympy import Symbol, Matrix, Function, simplify
 import numpy as np
 import sympy as sp
-
+#import pandas as pd
 
 class localelement_function_evaluation():
     def __init__(self,K,theta,K_prime,theta_prime,u,P_El):
@@ -59,10 +59,11 @@ class localelement_function_evaluation():
         # Initialize
         self.theta_in_Q = np.zeros((len(u),1))
         self.K_in_Q = np.zeros((len(u),1))
-        self.theta_prime_Q = np.zeros((len(u),1))
+        #self.theta_prime_Q = np.zeros((len(u),1))
         #self.K_d_theta = np.zeros((len(u),1))
         self.K_prime_Q = np.zeros((len(u),1))
-        #self.theta_prime_Q = theta_prime_func(self.val_Q)
+        self.theta_prime_Q = theta_prime_func(self.val_Q)
+        #self.theta_in_Q = theta_func(self.val_Q)
         #self.K_prime_Q = K_prime_func(self.theta_in_Q)*self.theta_prime_Q
         for k in range(len(u)):
             self.theta_in_Q[k] = theta_func(self.val_Q[k].item())
@@ -73,7 +74,7 @@ class localelement_function_evaluation():
             # z = self.theta_prime_func.subs(x,self.val_Q[k].item())
             # r = self.K_prime_func.subs(x,self.theta_in_Q[k].item())
 
-            self.theta_prime_Q[k] = theta_prime_func(self.val_Q[k].item())#self.theta_prime_func.evalf(subs={x: np.asscalar(self.val[k])})
+            #self.theta_prime_Q[k] = theta_prime_func(self.val_Q[k].item())#self.theta_prime_func.evalf(subs={x: np.asscalar(self.val[k])})
             #print(r)
             # derivative of theta wrt psi
             #self.K_d_theta[k] = r #self.K_prime_func(self.theta[k])
@@ -223,64 +224,48 @@ class localelement_function_evaluation_P():
         # matrix assembly, this would allow for specified functions determining what to do with each
         # they will both be part of the residual
         
-        self.val = u
-        self.theta_func = theta
-        self.theta_prime_func = theta_prime
-        self.K_func = K
-        
-        
+        theta_func = theta
         x = sp.symbols('x')
+        theta_prime_func =sp.lambdify([x],theta_prime)
+        K_func = K
+        #K_prime_func = sp.lambdify([x],K_prime)
+        
+        
+        
         
         quadrature = gauss_quadrature_points(P_El.degree +1)
         quadrature_points = quadrature[:,0:2]
         Phi = P_El.phi_eval(quadrature_points)
         dPhi = P_El.grad_phi_eval(quadrature_points)
-        self.val_Q = np.zeros((len(Phi),1))# np.dot(self.val.T.reshape(len(self.val)),Phi.T)
-        #self.valgrad_Q = np.zeros((dPhi.shape[1],dPhi.shape[2]))
+        # self.val_Q = np.zeros((len(Phi),1))# np.dot(self.val.T.reshape(len(self.val)),Phi.T)
+        # self.valgrad_Q = np.zeros((dPhi.shape[1],dPhi.shape[2]))
         # sum over quadrature points q
-        for q in range(len(Phi)):
-            
-            # sum over local index
-            rtalt = np.sum(np.multiply(self.val,Phi[:,q].reshape(-1,1)))
-           
-            self.val_Q[q] = rtalt
-            #self.valgrad_Q[q] = gradrt
-        # rtalt2 = np.dot(self.val,Phi)
-        # print(rtalt2,'valdiff',self.val_Q)
+        self.valgrad_Q = np.tensordot(u,dPhi,axes=((0),(1)))[0]
+        self.val_Q=np.sum(u*Phi,axis=0)
         # Initialize
-        self.theta = np.zeros((len(u),1))
-        self.K = np.zeros((len(u),1))
-        self.theta_d_psi = np.zeros((len(u),1))
+        self.theta_in_Q = np.zeros((len(u),1))
+        self.K_in_Q = np.zeros((len(u),1))
+        #self.theta_prime_Q = np.zeros((len(u),1))
         #self.K_d_theta = np.zeros((len(u),1))
-        #self.K_d_psi = np.zeros((len(u),1))
+        #self.K_prime_Q = np.zeros((len(u),1))
+        self.theta_prime_Q = theta_prime_func(self.val_Q)
+        #self.theta_in_Q = theta_func(self.val_Q)
+        #self.K_prime_Q = K_prime_func(self.theta_in_Q)*self.theta_prime_Q
         for k in range(len(u)):
-            self.theta[k] = self.theta_func(self.val_Q[k].item())
-            self.K[k]     = self.K_func(self.theta[k].item())
+            self.theta_in_Q[k] = theta_func(self.val_Q[k].item())
+            self.K_in_Q[k]     = K_func(self.theta_in_Q[k].item())
             
             #self.K[k]     = self.K_func(self.val_Q[k].item())
             #r = self.K_prime_func.subs(x,self.val_Q[k].item())
-            z = self.theta_prime_func.subs(x,self.val_Q[k].item())
-            #r = self.K_prime_func.subs(x,self.theta[k].item())
+            # z = self.theta_prime_func.subs(x,self.val_Q[k].item())
+            # r = self.K_prime_func.subs(x,self.theta_in_Q[k].item())
 
-            self.theta_d_psi[k] = z#self.theta_prime_func.evalf(subs={x: np.asscalar(self.val[k])})
+            #self.theta_prime_Q[k] = theta_prime_func(self.val_Q[k].item())#self.theta_prime_func.evalf(subs={x: np.asscalar(self.val[k])})
             #print(r)
             # derivative of theta wrt psi
             #self.K_d_theta[k] = r #self.K_prime_func(self.theta[k])
             # derivative of K wrt psi
-            #self.K_d_psi[k] = self.K_d_theta[k]*self.theta_d_psi[k]
-        
-        # TODO does nothing but keeps naming conventions 
-        self.interpolate_to_quadpts(P_El) 
-    
-    def interpolate_to_quadpts(self,P_El):
-        quadrature = gauss_quadrature_points(P_El.degree +1)
-        quadrature_points = quadrature[:,0:2]
-        Phi = P_El.phi_eval(quadrature_points)
-        self.K_in_Q = self.K#np.dot(self.K.T.reshape(len(self.K)),Phi.T)
-        self.theta_in_Q = self.theta#np.dot(self.theta.T.reshape(len(self.theta)),Phi.T)
-        #self.K_prime_Q = self.K_d_psi#np.dot(self.K_d_psi.T.reshape(len(self.K)),Phi)
-        self.theta_prime_Q = self.theta_d_psi#np.dot(self.theta_d_psi.T.reshape(len(self.theta)),Phi.T)
-        
+            #self.K_prime_Q[k] = K_prime_func(self.theta_in_Q[k].item())*self.theta_prime_Q[k]
                 
         
 class localelement_function_evaluation_L():
@@ -329,22 +314,23 @@ class localelement_function_evaluation_L():
         #     rtalt = np.sum(np.multiply(self.val,Phi[:,q].reshape(-1,1)))    
         #     self.val_Q[q] = rtalt
         #  # sum over local index
-        self.valgrad_Q = np.zeros((dPhi.shape[1],dPhi.shape[2]))
-        for q in range(len(Phi)):
+        # self.valgrad_Q = np.zeros((dPhi.shape[1],dPhi.shape[2]))
+        # for q in range(len(Phi)):
             
-            gradrt =0
-            # sum over local index
-            for k in range(len(self.val)):
+        #     gradrt =0
+        #     # sum over local index
+        #     for k in range(len(self.val)):
                 
-                gradrt += self.val[k]*dPhi[q][k]
-            #rtalt = np.sum(np.multiply(self.val,Phi[:,q].reshape(-1,1)))
-            #print(rt,rtalt)
-            #print(rtalt)
+        #         gradrt += self.val[k]*dPhi[q][k]
+        #     #rtalt = np.sum(np.multiply(self.val,Phi[:,q].reshape(-1,1)))
+        #     #print(rt,rtalt)
+        #     #print(rtalt)
             
-            #self.val_Q[q] = rtalt
-            self.valgrad_Q[q] = gradrt
+        #     #self.val_Q[q] = rtalt
+        #     self.valgrad_Q[q] = gradrt
         
-     
+        self.valgrad_Q = np.tensordot(self.val,dPhi,axes=((0),(1)))[0]
+        
         #  #self.valgrad_Q[q] = gradrt
         rtalt2 = np.sum(self.val*Phi,axis=0)
         #print(rtalt2,'valdiff',self.val_Q)  
@@ -410,7 +396,7 @@ class localelement_function_evaluation_norms():
         # Initialize
         self.theta_in_Q = np.zeros((len(u),1))
         self.K_in_Q = np.zeros((len(u),1))
-        self.theta_prime_Q = np.zeros((len(u),1))
+        #self.theta_prime_Q = np.zeros((len(u),1))
 
         
         self.theta_in_Q2 = np.zeros((len(u),1))
@@ -484,7 +470,7 @@ class localelement_function_evaluation_norms():
 
 
 class localelement_function_evaluation_norms2():
-    def __init__(self,K,theta,K_prime,theta_prime,val,val2,val3,P_El):
+    def __init__(self,K_func,theta_func,K_prime,theta_prime,val,val2,val3,P_El):
         '''
         
 
@@ -504,11 +490,11 @@ class localelement_function_evaluation_norms2():
         x = sp.symbols('x',real=True)
         
         
-        theta_func = theta
+  
         theta_prime_func = sp.lambdify([x],theta_prime)
-        K_func = K
+   
         K_prime_func = sp.lambdify([x],K_prime)
-        
+        #K_prime_func = K_prime
         
         
         
@@ -517,45 +503,53 @@ class localelement_function_evaluation_norms2():
         Phi = P_El.phi_eval(quadrature_points)
         dPhi = P_El.grad_phi_eval(quadrature_points)
        
-        
+        # dF = pd.DataFrame(val)
+        # self.theta_in_Q = dF.apply(theta_func)
+       
         self.val_Q=np.sum(val*Phi,axis=0)
         self.val_Q2=np.sum(val2*Phi,axis=0)
    
         # Initialize
         self.theta_in_Q = np.zeros((3,1),dtype=np.float64)
         self.K_in_Q = np.zeros((3,1),dtype=np.float64)
-        self.theta_prime_Q = np.zeros((3,1),dtype=np.float64)
+        #self.theta_prime_Q = np.zeros((3,1),dtype=np.float64)
 
         
         self.theta_in_Q2 = np.zeros((3,1),dtype=np.float64)
         self.K_in_Q2 = np.zeros((3,1),dtype=np.float64)
-        self.theta_prime_Q2 = np.zeros((3,1),dtype=np.float64)
+        #self.theta_prime_Q2 = np.zeros((3,1),dtype=np.float64)
         self.K_prime_Q2=np.zeros((3,1),dtype=np.float64)
     
         # sum over quadrature points q
         self.valgrad_Q = np.tensordot(val,dPhi,axes=((0),(1)))[0]
-       
+        self.valgrad_Q2 = np.tensordot(val2,dPhi,axes=((0),(1)))[0]
        
         self.valgrad_Q3 =np.tensordot(val3,dPhi,axes=((0),(1)))[0]
-        # self.theta_prime_Q = theta_prime_func(self.val_Q)
-        #self.theta_prime_Q2 = theta_prime_func(self.val_Q2)
+        self.theta_prime_Q = theta_prime_func(self.val_Q)
+        self.theta_prime_Q2 = theta_prime_func(self.val_Q2)
+        #self.K_prime_Q2 = K_prime_func(self.theta_in_Q2)*self.theta_prime_Q2
+        #self.theta_in_Q = theta_func(self.val_Q)
         for q in range(3):
             self.theta_in_Q[q] = theta_func(self.val_Q[q].item())
             self.K_in_Q[q]     = K_func(self.theta_in_Q[q].item())
   
    
 
-            self.theta_prime_Q[q] = theta_prime_func(self.val_Q[q].item())#self.theta_prime_func.subs(x,self.val_Q[q].item())
+            #self.theta_prime_Q[q] = theta_prime_func(self.val_Q[q].item())#self.theta_prime_func.subs(x,self.val_Q[q].item())
      
             
             
             self.theta_in_Q2[q] = theta_func(self.val_Q2[q].item())
             self.K_in_Q2[q]     = K_func(self.theta_in_Q2[q].item())
-            self.theta_prime_Q2[q] = theta_prime_func(self.val_Q2[q].item())#self.theta_prime_func.subs(x,self.val_Q2[q].item())#self.theta_prime_func.evalf(subs={x: np.asscalar(self.val[k])})
+            #self.theta_prime_Q2[q] = theta_prime_func(self.val_Q2[q].item())#self.theta_prime_func.subs(x,self.val_Q2[q].item())#self.theta_prime_func.evalf(subs={x: np.asscalar(self.val[k])})
             
 
-         
-            self.K_prime_Q2[q] = K_prime_func(self.theta_in_Q2[q].item())*self.theta_prime_Q2[q]#self.K_prime_func.subs(x,self.theta_in_Q2[q].item())*self.theta_prime_Q2[q]
+            #K_prime_func(self.theta_in_Q2[q].item())*self.theta_prime_Q2[q]#
+            # if self.theta_prime_Q2[q]==0 or self.theta_in_Q2[q].item()==0.131:
+            #     self.K_prime_Q2[q] =0
+            
+            # else:
+            self.K_prime_Q2[q] = K_prime_func(self.theta_in_Q2[q].item())*self.theta_prime_Q2[q]#K_prime_func.subs(x,self.theta_in_Q2[q].item())*self.theta_prime_Q2[q]
       
             
         
