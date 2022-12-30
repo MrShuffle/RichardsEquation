c5a728c1b6c148a2c5eb17f5fd2680339c42f4ab
jsstokke <71754696+MrShuffle@users.noreply.github.com>
2022-12-30 14:58:32 +0100

    Removed comments

--- RichardsEqFEM/source/basisfunctions/lagrange_element.py
+++ RichardsEqFEM/source/basisfunctions/lagrange_element.py
@@ -34,7 +34,7 @@ def vandermonde(points, degree):
 
     # Dimension of the ploynomial space in 2D
     dim_of_space = int((degree + 1) * (degree + 2) / 2)
-    # Initialize the matrix
+    # Initialize matrix
     V = np.ones((len(points), dim_of_space))
 
     for k in range(degree + 1):
@@ -103,7 +103,8 @@ class finite_element:
         self.poly_dim = int(
             (degree + 1) * (degree + 2) / 2
         )  # dimension of mononomial basis of polynomials
-
+        
+        # Coordinates of reference triangle
         self.pts = np.array([[0, 0], [1, 0], [0, 1]])
         # dictionary with local nodes
         self.local_dofs = {
@@ -111,57 +112,24 @@ class finite_element:
             "faces": {},
             "elements": {},
         }
-
+        
+        # Degrees of freedom per element
         self.num_dofs = 3
-        # # P1 element
-        # if self.degree == 1:
-
-        #     self.pts = np.array([[0,0],
-        #                           [1,0],
-        #                           [0,1]])
-        #     # dictionary with local nodes
-        #     self.local_dofs =  {'nodes': {0: [0],
-        #                              1: [2],
-        #                              2: [1]},
-        #                         'faces':{},
-        #                         'elements':{}}
-
-        #     self.num_dofs = 3
-
-        # # P2 element
-        # elif self.degree ==2:
-
-        #     self.pts = self.__lagrange_pts(self.degree)
-        #     self.pts = np.array([[0,0],
-        #                           [0.5,0],
-        #                           [1,0],
-        #                           [0.5,0.5],
-        #                           [0,1],
-        #                           [0,0.5]])
-        #     self.local_dofs =   {'nodes': {0: [0],
-        #                              1: [4],
-        #                              2: [2]},
-        #                          'faces': {0: [5],
-        #                              1: [1],
-        #                              2: [3]},
-        #                          'elements':{}}
-
-        #     self.num_dofs = 6
-        # else:
-        #     NotImplementedError()
-
+        
+        # Create vandermonde matrix on refrence triangle
         V = vandermonde(self.pts, self.degree)
-
+        
+        # Comptute inverse of Vandermonde matrix
         self.inv_V = np.linalg.inv(V)
         self.V = V
 
-    # Evaluate test functions
+    # Evaluate basis functions
     def phi_eval(self, points):
         V = vandermonde(points, self.degree)
 
         return V @ self.inv_V
 
-    # Evaluate gradient of test functions
+    # Evaluate gradient of basis functions
     def grad_phi_eval(self, points):
         dV = vandermonde_Grad(points, self.degree)
         gradPhi = np.empty((dV.shape[0], self.poly_dim, 2))
@@ -196,31 +164,15 @@ class global_element_geometry(finite_element):
         self.geometry = geometry
 
         self.element_coord = self.corners
-        # # P1 element
-        # if self.degree == 1:
-
-        #     self.element_coord = self.corners
-
-        # # P2 element
-        # elif self.degree ==2:
-
-        #     self.element_coord = self.mid_point(self.corners)
-        #     #print(self.element_coord)
-        #     #self.element_coord = self.coordinates_sorted(self.geometry,2)
-        #     #print(self.element_coord)
-
-        # else:
-        #     NotImplementedError()
-
-    # TODO: use porepy's g.face_centers instead.
+     
     def mid_point(self, corners):
 
         x_coord = corners[0]
         y_coord = corners[1]
-        # print(y_coord)
+        
         x_M = np.zeros(len(x_coord))
         y_M = np.zeros(len(y_coord))
-        itr = 0
+        
 
         x_M = (x_coord[1:] + x_coord[:-1]) / 2
         x_M = np.append(x_M, (x_coord[0] + x_coord[-1]) / 2)
@@ -236,5 +188,5 @@ class global_element_geometry(finite_element):
         y_coord2[idx_new] = y_coord2[idx]
 
         points = np.array([x_coord2, y_coord2])
-        # points =np.array([x_mod,y_mod])
+ 
         return points
